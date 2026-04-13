545c210c89439df4336737c6a1111638ed9636b1
墨染轻尘 <mor.ink2026@gmail.com>
2026-04-13 10:52:11 +0200

    Create capacitor.config.ts file with Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+// capacitor.config.ts
+
+import { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'Imposter',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
